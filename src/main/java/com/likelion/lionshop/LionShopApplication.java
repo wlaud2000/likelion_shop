7f13fcfbf5151597fe5111ef8172a0919a236254
Jimyeong Kim <kjm021221@icloud.com>
2024-03-26 19:16:55 +0900

    first commit

---
+++ src/main/java/com/likelion/lionshop/LionShopApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.lionshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/lionshop/LionShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.lionshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
