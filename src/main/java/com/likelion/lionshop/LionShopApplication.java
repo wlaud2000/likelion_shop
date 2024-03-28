11c14b18936762d78cd48d6098f1b7bd45824660
Jimyeong Kim <kjm021221@icloud.com>
2024-03-28 20:08:55 +0900

    0주차 과제

--- src/main/java/com/likelion/lionshop/LionShopApplication.java
+++ src/main/java/com/likelion/lionshop/LionShopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LionShopApplication {
+public class LionShopApplication { //0주차 과제
 
     public static void main(String[] args) {
         SpringApplication.run(LionShopApplication.class, args);
