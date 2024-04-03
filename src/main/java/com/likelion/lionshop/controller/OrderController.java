d6dea9e9e95a25470d8a3d8d6095d0c990901984
DDonghyeo <gsu9045000@gmail.com>
2024-04-03 14:04:13 +0900

    1주차 과제

---
+++ src/main/java/com/likelion/lionshop/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.likelion.lionshop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j //로그 출력을 도와주는 어노테이션
+@RestController
+@RequestMapping("/order") // uri가 /oroder로 시작하는 요청을 받습니다.
+public class OrderController {
+
+    // 1. 주문을 생성하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 생성하기"입니다. -> 주문은 리스트 형태로 요청을 보내주세요!
+    //CreateOrderRequestDto 클래스를 매개변수로 받습니다.
+    public String createOrder() {
+
+        return "";
+    }
+
+
+    // 2. 주문을 가져오는 컨트롤러를 만듭니다. 이때 return 값은 "주문 가져오기"입니다.
+    public String getOrder() {
+
+        return "";
+    }
+
+    // 3. 주문을 수정하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 수정하기"입니다.
+    //UpdateOrderRequestDto 클래스를 매개변수로 받습니다.
+    public String updateOrder() {
+
+        return "";
+    }
+
+    // 4. 주문을 삭제하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 삭제하기"입니다.
+    public String deleteOrder() {
+
+        return "";
+    }
+
+}
---
+++ src/main/java/com/likelion/lionshop/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.likelion.lionshop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j //로그 출력을 도와주는 어노테이션
+@RestController
+@RequestMapping("/user") // uri가 /user로 시작하는 요청을 받습니다.
+public class UserController {
+
+    // 1. 사용자를 생성하는 컨트롤러를 만듭니다.
+    // 이때 log.info를 이용하여 사용자의 이름, 주소, ID, PW를 출력해줍니다. return 값은 "사용자 생성"입니다.
+    //CreateUserRequestDto 클래스를 매개변수로 받습니다.
+    public String createUser() {
+        return "";
+    }
+
+    // 2. 사용자를 조회하는 컨트롤러를 만듭니다.
+    // 이때 log.info를 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 조회"입니다.
+    public String getUser() {
+        return "";
+    }
+
+    // 3. 사용자를 수정하는 컨트롤러를 만듭니다.
+    // 이때 log.info를 이용를여 사용자의 이름, 주소를 출력해줍니다. return 값은 "사용자 수정"입니다.
+    //UpdateUserRequestDto 클래스를 매개변수로 받습니다.
+    public String updateUser() {
+        return "";
+    }
+
+    // 4. 사용자를 삭제하는 컨트롤러를 만듭니다.
+    // 이때 log.info 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 삭제"입니다.
+    public String deleteUser() {
+        return "";
+    }
+
+}
