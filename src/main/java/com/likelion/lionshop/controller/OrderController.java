05bdce059175aecf7792a8765ae86d73a3ebaf1c
macbookpro <macbookpro@Jimyeong-MacBook-Pro.local>
2024-05-12 22:14:20 +0900

    4주차 과제 수정

--- src/main/java/com/likelion/lionshop/controller/OrderController.java
+++ src/main/java/com/likelion/lionshop/controller/OrderController.java
@@ -26,9 +26,6 @@ public class OrderController {
     @PostMapping(value = "")
     public ResponseEntity<List<OrderResponseDto>> createOrders(@RequestBody List<CreateOrderRequestDto> createOrderRequestDtos,
                                                                @AuthenticationPrincipal UserDetails userDetails) { //List형태로 주문을 받을 수 있게 수정
-        if(userDetails == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
 
         List<OrderResponseDto> orderResponseDtos = orderService.createOrders(createOrderRequestDtos);
         return new ResponseEntity<>(orderResponseDtos, HttpStatus.CREATED);
@@ -39,9 +36,6 @@ public class OrderController {
     @GetMapping(value = "/{orderId}")
     public ResponseEntity<OrderResponseDto> getOrder(@PathVariable("orderId") Long id,
                                                      @AuthenticationPrincipal UserDetails userDetails) {
-        if(userDetails == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
 
         OrderResponseDto orderResponseDto = orderService.getOrder(id);
         return new ResponseEntity<>(orderResponseDto,HttpStatus.OK);
@@ -50,26 +44,18 @@ public class OrderController {
     // 3. 주문을 수정하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 수정하기"입니다.
     //UpdateOrderRequestDto 클래스를 매개변수로 받습니다.
     @PutMapping("/{orderId}")
-    public String updateOrder(@PathVariable("orderId") Long id,
-                              @RequestBody UpdateOrderRequestDto updateOrderRequestDto,
-                              @AuthenticationPrincipal UserDetails userDetails) {
-        if(userDetails == null) {
-            return "로그인이 필요합니다.";
-        }
-
+    public ResponseEntity<String> updateOrder(@PathVariable("orderId") Long id,
+                                              @RequestBody UpdateOrderRequestDto updateOrderRequestDto,
+                                              @AuthenticationPrincipal UserDetails userDetails) {
         orderService.updateOrder(id, updateOrderRequestDto);
-        return "주문 수정";
+        return new ResponseEntity<>("주문 수정", HttpStatus.OK);
     }
 
     // 4. 주문을 삭제하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 삭제하기"입니다.
     @DeleteMapping(value = "/{orderId}")
-    public String deleteOrder(@PathVariable("orderId") Long id,
-                              @AuthenticationPrincipal UserDetails userDetails) {
-        if(userDetails == null) {
-            return "로그인이 필요합니다.";
-        }
-
+    public ResponseEntity<String> deleteOrder(@PathVariable("orderId") Long id,
+                                              @AuthenticationPrincipal UserDetails userDetails) {
         orderService.deleteOrder(id);
-        return "주문 삭제하기";
+        return new ResponseEntity<>("주문 삭제하기", HttpStatus.OK);
     }
 }
